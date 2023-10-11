c4960a5d42471cab77410fa3937ba0e93f042e62
uRIELoLIVERO <oliverouri4@gmail.com>
2023-10-11 16:01:02 -0300

    a

--- moon-project/NumerosYLetras.java
+++ moon-project/NumerosYLetras.java
@@ -1,10 +1,15 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 public class NumerosYLetras extends Actor
 {
-    int imageWidth = 120; // Ancho en píxeles
-    int imageHeight = 120; // Altura en px
-    private String[] rutasImagenes = {"ocho.png","cuatro.png","dos.png","cuatro.png","D.png","B.png","C.png"}; 
-    public NumerosYLetras(String rutaImagen){
+    /**
+     * Act - do whatever the Numeros wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    int imageWidth = 140; // Ancho en píxeles
+    int imageHeight = 140; // Altura en px
+    //imageArray[] = new GreenfootImage[4];
+    private String[] rutasImagenes = {"ocho.png","cuatro.png","dos.png","cuatro.png"}; 
+    public Numeros(String rutaImagen){
         GreenfootImage imagen = new GreenfootImage(rutaImagen);
         setImage(imagen);
         imagen.scale(imageWidth,imageHeight);
