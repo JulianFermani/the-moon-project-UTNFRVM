ce6c4e497f939b94cdd69aaaefa106123e5f6f03
afalchi <alexandermassietti@gmail.com>
2023-10-12 15:05:33 -0300

    Implementación OR testeada

--- moon-project/Or.java
+++ moon-project/Or.java
@@ -1,4 +1,4 @@
-import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
 
 public class Or extends Operaciones
 {
@@ -15,26 +15,25 @@ public class Or extends Operaciones
     }
     public void checkKeys()
     {
-         if (Greenfoot.isKeyDown("7")) {
-            fila1 = elegirFila();
-            fila2 = elegirFila();
-            imageWidth = 110;
-            imageHeight = 110;
-            for (int i = 7; i > 3; i--) {
-                // Se obtienen los dos objetos de interés por fila y columna.
-                objeto1 = buscarObjetoEnCoordenadas(i, fila1);
-                objeto2 = buscarObjetoEnCoordenadas(i, fila2);
+       if (Greenfoot.isKeyDown("7")) {
+        fila1 = elegirFila();
+        fila2 = elegirFila();
+        imageWidth = 110;
+        imageHeight = 110;
+        for (int i = 7; i > 3; i--) {
+            // Se obtienen los dos objetos de interés por fila y columna.
+            objeto1 = buscarObjetoEnCoordenadas(i, fila1);
+            objeto2 = buscarObjetoEnCoordenadas(i, fila2);
 
-                if (objeto1.booleano == 1 || objeto2.booleano == 1) {
-                    objeto1.booleano = 1;
-                    objeto1.setImage("art_7.png"); // Cambiar a la imagen correspondiente a verdadero
-                } else {
-                    objeto1.booleano = 0;
-                    objeto1.setImage("art_8.png"); // Cambiar a la imagen correspondiente a falso
-                }
-                GreenfootImage image = objeto1.getImage();
-                image.scale(imageWidth, imageHeight);
-        }
+            if (objeto1.booleano == 1 || objeto2.booleano == 1) {
+                objeto1.booleano = 1; // Cambiar el estado de objeto1 antes que objeto2
+                objeto1.setImage("art_7.png"); // Cambiar a la imagen correspondiente a verdadero
+            } else {
+                objeto1.booleano = 0;
+                objeto1.setImage("art_8.png"); // Cambiar a la imagen correspondiente a falso
+            }
+            GreenfootImage image = objeto1.getImage(); // Utiliza objeto1 en lugar de objeto2
+            image.scale(imageWidth, imageHeight);        }
     }
 }
 }
