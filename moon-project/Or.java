0bed1c8ce9a6e805d5472778a1a4ea9bc7540a73
afalchi <alexandermassietti@gmail.com>
2023-10-11 21:05:09 -0300

    Chequear la nueva implementación OR

--- moon-project/Or.java
+++ moon-project/Or.java
@@ -8,56 +8,33 @@ public class Or extends Operaciones
         GreenfootImage image = getImage();
         image.scale(imageWidth,imageHeight);
     }
-      public void act()
+ 
+   public void act()
     {
         checkKeys();
     }
     public void checkKeys()
     {
-  for (int columna = 4; columna <= 7; columna++) { // Iterar a través de todas las columnas
-    if (Greenfoot.isKeyDown("7")) {
-        int estado = 0;
+         if (Greenfoot.isKeyDown("7")) {
+            fila1 = elegirFila();
+            fila2 = elegirFila();
+            imageWidth = 110;
+            imageHeight = 110;
+            for (int i = 7; i > 3; i--) {
+                // Se obtienen los dos objetos de interés por fila y columna.
+                objeto1 = buscarObjetoEnCoordenadas(i, fila1);
+                objeto2 = buscarObjetoEnCoordenadas(i, fila2);
 
-        while (true) {
-            if (estado == 0) {
-                if (Greenfoot.isKeyDown("D")) {
-                    estado = 1;
-                } else if (Greenfoot.isKeyDown("C")) {
-                    estado = 2;
-                } else if (Greenfoot.isKeyDown("B")) {
-                    estado = 3;
+                if (objeto1.booleano == 1 || objeto2.booleano == 1) {
+                    objeto1.booleano = 1;
+                    objeto1.setImage("art_7.png"); // Cambiar a la imagen correspondiente a verdadero
+                } else {
+                    objeto1.booleano = 0;
+                    objeto1.setImage("art_8.png"); // Cambiar a la imagen correspondiente a falso
                 }
-            }
-
-            if (estado > 0) {
-                if (Greenfoot.isKeyDown("B") || Greenfoot.isKeyDown("C") || Greenfoot.isKeyDown("D")) {
-                    int filaATrabajar = 0; // Inicializar fila en 0
-
-                    if (estado == 1 && Greenfoot.isKeyDown("D")) {
-                        filaATrabajar = 1;
-                    } else if (estado == 2 && Greenfoot.isKeyDown("C")) {
-                        filaATrabajar = 2;
-                    } else if (estado == 3 && Greenfoot.isKeyDown("B")) {
-                        filaATrabajar = 3;
-                    }
-
-                    // Realiza la operación OR en función de filaATrabajar y columna
-                    objeto = buscarObjetoEnCoordenadas(columna, filaATrabajar);
-
-                    if (objeto != null) {
-                        int booleanoDelObjeto = objeto.booleano;
-                        objeto.booleano = (booleanoDelObjeto == 0) ? 1 : 0;
-                        objeto.setImage((booleanoDelObjeto == 0) ? "art_7.png" : "art_8.png");
-                        GreenfootImage image = objeto.getImage();
-                        image.scale(imageWidth, imageHeight);
-                    }
-
-                    break; // Sal del bucle después de realizar la operación OR
-                }
-            }
+                GreenfootImage image = objeto1.getImage();
+                image.scale(imageWidth, imageHeight);
         }
     }
-}
-
 }
 }
