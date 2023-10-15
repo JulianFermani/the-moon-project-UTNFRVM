b68050e9e7e7f4f0b5634a86edcf73b9fe9867e2
Julián <julianfermani45@gmail.com>
2023-10-15 14:55:41 -0300

    Realización de la operación desplazar a la derecha.

--- moon-project/DespDerecha.java
+++ moon-project/DespDerecha.java
@@ -1,7 +1,12 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+import java.util.*;
 
 public class DespDerecha extends Operaciones
 {
+    Zero_One objeto;
+    List<Zero_One> vectorObjetosFila = new ArrayList<Zero_One>();
+    int i, booleanoUltimo;
+    GreenfootImage imagenUltimo, imagen;
     public DespDerecha()
     {
         setImage("ROR.png");
@@ -10,6 +15,47 @@ public class DespDerecha extends Operaciones
     }
     public void act()
     {
-        // Add your action code here.
+        checkKeys();
+    }
+    public void checkKeys()
+    {
+        if (Greenfoot.isKeyDown("4"))
+        {
+            columnaATrabajar = 7; // Se sabe que la columna a trabajar es igual a coordenadaX.
+            filaATrabajar = elegirFila();
+            
+            if (esperando) // Solo entra si se toco alguna tecla de interes.
+            {
+              vectorObjetosFila = obtenerVectorObjetos(filaATrabajar);
+              booleanoUltimo= vectorObjetosFila.get(vectorObjetosFila.size() - 1).booleano;
+              imagenUltimo = vectorObjetosFila.get(vectorObjetosFila.size() - 1).getImage();
+              for (i = vectorObjetosFila.size() - 1; i > 0; i--)
+              {
+                vectorObjetosFila.get(i).booleano = vectorObjetosFila.get(i-1).booleano;
+                imagen = vectorObjetosFila.get(i-1).getImage();
+                vectorObjetosFila.get(i).setImage(imagen); 
+                
+              }
+              vectorObjetosFila.get(0).booleano = booleanoUltimo;
+              vectorObjetosFila.get(0).setImage(imagenUltimo);
+            }           
+        }    
+    }
+    public List<Zero_One> obtenerVectorObjetos(int yObjetivo)
+    {
+        World mundo = getWorld(); // Obtén una referencia al mundo actual
+        List<Zero_One> objetosEnY = new ArrayList<Zero_One>();
+        
+        List<Zero_One> objetos = mundo.getObjects(Zero_One.class); // Obtiene todos los objetos de la clase Zero_One en el mundo
+        
+        for (Zero_One objeto : objetos)
+        {
+            if (objeto.getY() == yObjetivo)
+            {
+                objetosEnY.add(objeto); // Agrega el objeto encontrado en las coordenadas Y específicas al array
+            }
+        }
+        
+        return objetosEnY; // Devuelve el array de objetos encontrados en las coordenadas Y específicas
     }
 }
